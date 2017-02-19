9a025636c692a6ea5f1a3f3b1acec707cb02b789
Absurdius <e94doclrd@hotmail.com>
2017-02-19 15:30:17 +0100

    almost done with A3

--- lab3/date.cc
+++ lab3/date.cc
@@ -1,7 +1,33 @@
 #include <ctime>  // time and localtime
 #include "date.h"
+#include <istream>
+#include <ostream>
 
 int Date::daysPerMonth[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+using namespace std;
+
+//operator override. Use & to indicate reference. 
+//use variable name without & in operator method.
+//returns an istream to allow caining of severat dates
+//Date value is "returned implicitly"
+istream& operator >> (istream &i, Date date)
+{
+	int y; 
+	i >> y;
+	i.ignore(1,'-');
+	int m; 
+	i >> m;
+	i.ignore(1,'-');
+	int d; 
+	i >> d;
+	date = Date(y, m, d);
+	return i;
+}
+
+ostream& operator << (ostream &o, Date date)
+{
+	return o << date.getYear() << "-" << date.getMonth() << "-" << date.getDay(); 
+}
 
 Date::Date() {
 	time_t timer = time(0); // time in seconds since 1970-01-01
@@ -16,17 +42,32 @@ Date::Date(int y, int m, int d)
 	year = y, month = m, day = d;
 }
 
-int Date::getYear() const {
-	return 0;
+int Date::getYear() const 
+{
+	return year;
 }
 
-int Date::getMonth() const {
-	return 0;
+int Date::getMonth() const 
+{
+	return month;
 }
 
-int Date::getDay() const {
-	return 0;
+int Date::getDay() const 
+{
+	return day;
 }
 
-void Date::next() {
+void Date::next() 
+{
+	day++; 
+	if(day > daysPerMonth[month])
+	{
+		day = 1; 
+		month++;
+		if(month > 12)
+		{
+			month = 1;
+			year++;
+		}
+	}
 }
