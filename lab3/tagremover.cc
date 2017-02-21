42ae4859b3b4b5cb3f0446d4e865f753c79efd8f
Axel Wihlborg <dat12awi@student.lu.se>
2017-02-21 16:25:19 +0100

    update tag

--- lab3/tagremover.cc
+++ lab3/tagremover.cc
@@ -11,26 +11,25 @@ Tagremover::Tagremover(istream& in) : instream(in){
 }
 
 void Tagremover::print(ostream& out) {
-  string input;
+  string change;
   string line;
 
-  cout << "Input file to change, type stop to type directly into program" << endl;
+  cout << "change file to change, type stop to type directly into program" << endl;
   string testfile;
   instream >> testfile;
   if(testfile != "stop"){
   ifstream test (testfile);
   cout << "Read from file complete" << endl;
 
-  while (getline(test, line)) input += line + "\n";
+  while (getline(test, line)) change += line + "\n";
   }else{
-  while (getline(instream, line)) input += line + "\n";
+  while (getline(instream, line)) change += line + "\n";
   }
-  input = regex_replace(input, regex("<[^<>$]*>"), "");
-  /* input = regex_replace(input, regex("<[^<>$]*>"), ""); */
-  input = regex_replace(input, regex("&lt;"), "<");
-  input = regex_replace(input, regex("&gt;"), ">");
-  input = regex_replace(input, regex("&nbsp;"), " ");
-  input = regex_replace(input, regex("&amp;"), "&");
+  change = regex_replace(change, regex("<[^<>$]*>"), "");
+  change = regex_replace(change, regex("&lt;"), "<");
+  change = regex_replace(change, regex("&gt;"), ">");
+  change = regex_replace(change, regex("&nbsp;"), " ");
+  change = regex_replace(change, regex("&amp;"), "&");
 
-  out << input;
+  out << change;
 }
