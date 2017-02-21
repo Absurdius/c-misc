9ebe4bcb55228cf5df74f97bf993f66ca4d7b1a1
Axel Wihlborg <dat12awi@student.lu.se>
2017-02-21 16:14:01 +0100

    Updated tagremover

--- lab3/tagremover.cc
+++ lab3/tagremover.cc
@@ -1,24 +1,36 @@
-//tag remover
 #include <iostream>
+#include <fstream>
 #include "tagremover.h"
+#include <regex>
 
 using namespace std;
 
-Tagremover::Tagremover(istream &is)
-{
-  outline = "";
-  string str ((istream_iterator<char>(is), istream_iterator<char>()));
-  char c;
-  while(is.get())
-  {
-    if(&c == "<")
-    {
-      cout << "found tag";
-    }
-  }
+Tagremover::Tagremover(istream& in) : instream(in){
+
+
 }
 
-void Tagremover::print(ostream &os)
-{
-  os& << outline;
+void Tagremover::print(ostream& out) {
+  string input;
+  string line;
+
+  cout << "Input file to change, type stop to type directly into program" << endl;
+  string testfile;
+  instream >> testfile;
+  if(testfile != "stop"){
+  ifstream test (testfile);
+  cout << "Read from file complete" << endl;
+
+  while (getline(test, line)) input += line + "\n";
+  }else{
+  while (getline(instream, line)) input += line + "\n";
+  }
+  input = regex_replace(input, regex("<[^<]*>[>$]"), "");
+  /* input = regex_replace(input, regex("<[^<>$]*>"), ""); */
+  input = regex_replace(input, regex("&lt;"), "<");
+  input = regex_replace(input, regex("&gt;"), ">");
+  input = regex_replace(input, regex("&nbsp;"), " ");
+  input = regex_replace(input, regex("&amp;"), "&");
+
+  out << input;
 }
--- lab3/tagremover.h
+++ lab3/tagremover.h
@@ -1,13 +1,18 @@
 #ifndef TAGREMOVER_H
 #define TAGREMOVER_H
+
+#include <string>
 #include <iostream>
+#include <fstream>
 
 class Tagremover {
-public:
-	Tagremover(std::istream &is);
-	void print(std::ostream &os);
-private:
-	std::string outline;
+ public:
+  Tagremover(std::istream& in);
+  void print(std::ostream& out);
+
+ private:
+  std::istream& instream;
+	std::ifstream test;
 };
 
 #endif
